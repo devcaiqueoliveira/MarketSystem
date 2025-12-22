2b0af9b1e8f3e41e58a54b9db8739cc8d5c03d11
Caique <devcaiqueoliveira@gmail.com>
2025-12-22 10:48:04 -0300

    first commit

---
+++ src/main/java/br/com/bahas/market/Market.java
@@ -0,0 +1,17 @@
+package br.com.bahas.market;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Market extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
